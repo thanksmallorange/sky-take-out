c3ab23b6732b192c44091283eb2182b048c3a153
“tso” <xiexc2020@163.com>
2025-06-23 09:35:48 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -20,8 +22,18 @@ public interface EmployeeMapper {
      * 插入员工数据
      * @param employee
      */
-    @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)" +
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, status," +
+            "create_time, update_time, create_user, update_user)" +
             "values " +
-            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status}," +
+            " #{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
+
+
+    /**
+     * 分页查询数据
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
